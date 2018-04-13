936c98374e47250f8cf2852494ff30a003d0d2d9
yexiaoxiaogo <841079936@qq.com>
2018-04-13 16:58:25 +0800

    init

---
+++ FileUplord/src/main/java/io/github/yexiaoxiaogo/FileUplord/App.java
@@ -0,0 +1,14 @@
+package io.github.yexiaoxiaogo.FileUplord;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *file uplord
+ *
+ */
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
